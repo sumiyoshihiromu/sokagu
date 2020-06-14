7e361942af6cc7eac0c5a16daf5d84eda0092698
EC2 Default User <ec2-user@ip-172-31-4-192.ap-northeast-1.compute.internal>
2020-06-14 03:53:42 +0000

    Add basic title

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
     get root_url
     assert_response :success
-    assert_select "title", "Top | SOKAGU"
+    assert_select "title", "SOKAGU"
   end
   
   test "should get about" do
