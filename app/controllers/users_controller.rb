b4d8df81ba253ea802436adec39f858d25ef2485
EC2 Default User <ec2-user@ip-172-31-4-192.ap-northeast-1.compute.internal>
2020-06-24 13:57:32 +0000

    ミス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user, {only: [:show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+  
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -9,8 +13,9 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.jpg")
+    @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.jpg", password_digest: params[:password_digest])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -37,6 +42,37 @@ class UsersController < ApplicationController
       redirect_to("/users/#{@user.id}")
     else
       render("users/edit")
+    end
+  end
+  
+  def login_form
+    
+  end
+  
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] ="権限がありません"
+      redirect_to("/posts/index")
     end
   end
   
